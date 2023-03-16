c6345991726c60f74ec0e6d55879d5ca14e2f845
Yao Zhang <alanzhangyao@outlook.com>
2023-03-15 23:18:06 -0400

    Add container command

--- models/index.js
+++ models/index.js
@@ -14,10 +14,13 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+    }
   );
 }
 
